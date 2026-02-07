aeacfaef4939789634f7549cbaa0495bf59b2ea9
gitzenlab <dr.leaf@proton.me>
2026-02-07 16:55:11 +0530

    Update main.py

--- eye/main.py
+++ eye/main.py
@@ -1,1 +1,15 @@
+import gc
+import machine
+import time
 
+gc.collect()
+gc.enable()
+
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+while True:
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    time.sleep(0.5)
