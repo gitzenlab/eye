fd6b0324916768976def54d28d7af12bac6c8113
gitzenlab <dr.leaf@proton.me>
2026-02-07 16:53:52 +0530

    Update boot.py

--- eye/boot.py
+++ eye/boot.py
@@ -1,1 +1,25 @@
+import ota
+import machine
+import network
 
+# github repository access and files to update
+ota = ota.ota(
+  user="gitzenlab",
+  repo="eye",
+  branch="main",
+  working_dir="eye",
+  files = ["boot.py", "main.py"]
+)
+
+try:
+    ota.wificonnect()
+    if ota.update():
+        print("rebooting...")
+        machine.reset()
+    else:
+        # deactivate micropython ap
+        ap = network.WLAN(network.AP_IF)
+        if ap.active():
+            ap.active(False)
+except OSError as e:
+    print(f"error: {e}")
