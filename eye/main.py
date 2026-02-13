cb38c6172a90b7ae30cac2eceafcc9225ab084d4
gitzenlab <atrikasaha4@gmail.com>
2026-02-13 10:29:44 +0530

    Update main.py

--- eye/main.py
+++ eye/main.py
@@ -9,7 +9,7 @@ gc.enable()
 led = machine.Pin(2, machine.Pin.OUT)
 spi = machine.SPI(2, baudrate=10000000, polarity=1, phase=0, sck=machine.Pin(18), mosi=machine.Pin(23))
 cs = machine.Pin(5, machine.Pin.OUT)
-display = dotmatrix(spi, cs, 4)
+display = dotmatrix.dotmatrix(spi, cs, 4)
 
 display.brightness(5)
 display.clear()
